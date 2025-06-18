ffe6214caba5e19744998f5698672dfa8fbd36dc
Michael Steven Rodriguez Lozano <msrodriguez@procibernetica.com>
2025-06-18 10:51:21 -0500

    update files

--- main.tf
+++ main.tf
@@ -1,24 +1,9 @@
-provider "google" {
-    project =  var.project
-    region = var.region
-    zone = var.zone
+module "vpc_network" {
+  source = "./modules/vpc"
+
+  project     = "tst"
+  region      = "tst"
+  zone        = "tst"
+  vpc_name    = "tst"
+  subnet_name = "tst"
 }
-
-resource "google_compute_network" "vpc_network" {
-   
-  name = var.vpc_name
-  auto_create_subnetworks =  false
-
-
-}
-
-resource "google_compute_subnetwork" "subnet" {
-  
-    name = var.subnet_name
-    ip_cidr_range = "10.0.0.0/24"
-    region = var.region
-    network =  google_compute_network.vpc_network
-    depends_on = [ google_compute_network.vpc_network]    
-    
-}
-
